b9d7419ea7c927a5bd33a759a9c886720f8c7cd8
Mality <emir77766@mail.ru>
2022-06-22 13:11:25 +0300

    validationException

--- src/main/java/com/emir/megamarket/web/error/handler/ShopUnitExceptionHandler.java
+++ src/main/java/com/emir/megamarket/web/error/handler/ShopUnitExceptionHandler.java
@@ -2,6 +2,7 @@ package com.emir.megamarket.web.error.handler;
 
 import com.emir.megamarket.web.error.Error;
 import com.emir.megamarket.web.error.ShopUnitNotFoundException;
+import com.emir.megamarket.web.error.ShopUnitValidationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +14,7 @@ import javax.validation.ConstraintViolationException;
 @ControllerAdvice
 public class ShopUnitExceptionHandler {
 
-    @ExceptionHandler(value = {ConstraintViolationException.class, MethodArgumentNotValidException.class})
+    @ExceptionHandler(value = {ConstraintViolationException.class, MethodArgumentNotValidException.class, ShopUnitValidationException.class})
     protected ResponseEntity<Object> handleConstraintViolation(Exception ex, WebRequest request) {
         return ResponseEntity.status(200).body(new Error(200, "Validation Failed"));
     }
